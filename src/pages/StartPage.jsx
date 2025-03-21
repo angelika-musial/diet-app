fb89850fb2f79983c9b94de4f5a31c100af2cb0e
Angelika Musiał <angelikamusial@tlen.pl>
2025-03-22 00:51:31 +0100

    refactor: integrate form validation in StartPage
    - Replace native form handling with react-hook-form and Zod validation
    - Add validation schema for login form
    - Display error messages for invalid inputs
    - Disable submit button while submitting
    - Adjust `.title` margin-bottom in StartPage.module.scss for better spacing

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,11 +1,29 @@
 import styles from './StartPage.module.scss';
 import logo from '../assets/logo.png';
-
 import Input from '../components/Input/Input';
 import Button from '../components/Button/Button';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { loginSchema } from '../utils/validation';
 
 export default function StartPage() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm({
+		resolver: zodResolver(loginSchema),
+	});
+
+	const onSubmit = async (data) => {
+		try {
+			console.log('Dane logowania:', data);
+		} catch (error) {
+			console.error('Błąd logowania:', error);
+		}
+	};
+
 	return (
 		<div className={styles.container}>
 			<img src={logo} alt='CalorIQ logo' className={styles.logo} />
@@ -17,22 +35,27 @@ export default function StartPage() {
 			<div className={styles.formSection}>
 				<div>
 					<h3 className={styles.title}>Zaloguj się</h3>
-					<form className={styles.form}>
+					<form
+						onSubmit={handleSubmit(onSubmit)}
+						className={styles.form}
+						noValidate
+						aria-busy={isSubmitting}
+					>
 						<Input
 							type='email'
 							id='email'
-							name='email'
 							placeholder='Adres e-mail'
-							required
+							{...register('email')}
+							error={errors.email?.message}
 						/>
 						<Input
 							type='password'
 							id='password'
-							name='password'
 							placeholder='Hasło'
-							required
+							{...register('password')}
+							error={errors.password?.message}
 						/>
-						<Button type='submit' variant='primary'>
+						<Button disabled={isSubmitting} type='submit'>
 							Zaloguj się
 						</Button>
 					</form>
