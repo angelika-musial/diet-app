ee311f15671be97301ab6dd275f4e649760eaad8
Angelika Musiał <angelikamusial@tlen.pl>
2025-03-22 00:57:53 +0100

    refactor: add form validation and style improvements to SignupForm
    - Integrate react-hook-form and Zod for signup validation
    - Display error messages for invalid inputs
    - Disable submit button while submitting
    - Adjust form layout and styling in SignupForm.module.scss
      - Increase max-width of the form
      - Adjust input spacing and positioning
      - Make submit button stick to the bottom

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,36 +1,76 @@
 import Input from '../Input/Input';
 import Button from '../Button/Button';
 import styles from './SignupForm.module.scss';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { signupSchema } from '../../utils/validation';
 
 export default function SignupForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm({
+		resolver: zodResolver(signupSchema),
+	});
+
+	const onSubmit = async (data) => {
+		try {
+			console.log('Dane rejestracyjne:', data);
+		} catch (error) {
+			console.error('Błąd rejestracji:', error);
+		}
+	};
+
 	return (
 		<div className={styles.container}>
-			<form className={styles.form}>
-				<h1 className={styles.header}>Załóż konto</h1>
-				<Input placeholder='Imię*' type='text' id='name' name='name' required />
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className={styles.form}
+				noValidate
+				aria-busy={isSubmitting}
+			>
+				<h1>Załóż konto</h1>
+				<Input
+					placeholder='Imię*'
+					type='text'
+					id='name'
+					{...register('name')}
+					error={errors.name?.message}
+				/>
 				<Input
 					placeholder='Nazwisko*'
 					type='text'
 					id='surname'
-					name='surname'
-					required
+					{...register('surname')}
+					error={errors.surname?.message}
 				/>
-				<Input placeholder='E-mail*' type='email' id='email' name='email' required />
+				<Input
+					placeholder='E-mail*'
+					type='email'
+					id='email'
+					{...register('email')}
+					error={errors.email?.message}
+				/>
 				<Input
 					placeholder='Hasło*'
 					type='password'
 					id='password'
-					name='password'
-					required
+					{...register('password')}
+					error={errors.password?.message}
 				/>
 				<Input
 					placeholder='Powtórz hasło*'
 					type='password'
 					id='confirmPassword'
-					name='confirmPassword'
-					required
+					{...register('confirmPassword')}
+					error={errors.confirmPassword?.message}
 				/>
-				<Button type='submit' variant='primary'>
+				<Button
+					disabled={isSubmitting}
+					type='submit'
+					className={styles.submitButton}
+				>
 					Zarejestruj się
 				</Button>
 			</form>
