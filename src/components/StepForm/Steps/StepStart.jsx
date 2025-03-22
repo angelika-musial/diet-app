299944bd381b59d6032539f04ec30ee438e2a895
Angelika Musiał <angelikamusial@tlen.pl>
2025-03-22 01:22:45 +0100

    Update StepStart component text

--- src/components/StepForm/Steps/StepStart.jsx
+++ src/components/StepForm/Steps/StepStart.jsx
@@ -4,10 +4,11 @@ export default function StepStart() {
 	return (
 		<Step title='Już prawie gotowe!'>
 			<p>
-				Potrzebujemy jeszcze kilku dodatkowych informacji, aby dokładnie obliczyć Twoje
-				dzienne zapotrzebowanie kaloryczne. W kolejnych krokach poprosimy Cię o
-				podanie płci, daty urodzenia, wagi, wzrostu oraz poziomu aktywności
-				fizycznej. Dzięki temu dostosujemy wyniki idealnie do Twoich potrzeb!
+				Potrzebujemy jeszcze kilku dodatkowych informacji, aby dokładnie
+				obliczyć Twoje dzienne zapotrzebowanie kaloryczne. W kolejnych krokach
+				poprosimy Cię o podanie płci, daty urodzenia, wagi, wzrostu, twojego
+				celu dietetycznego oraz poziomu aktywności fizycznej. Dzięki temu
+				dostosujemy wyniki idealnie do Twoich potrzeb!
 			</p>
 		</Step>
 	);
