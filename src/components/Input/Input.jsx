3cfcf057ee54b97fe4d5010e34e53e1b779c8d93
Angelika Musiał <angelikamusial@tlen.pl>
2025-06-12 22:55:35 +0200

    simplify Input component structure and styles

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,15 +1,15 @@
 import styles from './Input.module.scss';
 import { CircleAlert } from 'lucide-react';
 
-const Input = ({ label, id, className, error, ...props }) => {
+const Input = ({ label, id, className = '', error, ...props }) => {
 	return (
 		<>
-			<div className={styles.inputWrapper}>
+			<div>
 				<label htmlFor={id}>{label}</label>
 				{error && <p className={styles.error}><CircleAlert size={14} /> {error}</p>}
 				<input
 					id={id}
-					className={`${styles.inputField} ${
+					className={`${styles.input} ${
 						error ? styles.inputError : ''
 					} ${className}`}
 					{...props}
