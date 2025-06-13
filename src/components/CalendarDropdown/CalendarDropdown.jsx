9c378a9be95495885e740547ffd841ccf1b71ed3
Angelika Musiał <angelikamusial@tlen.pl>
2025-06-13 22:12:44 +0200

    fetch daily data on calendar date change using getProductsData

--- src/components/CalendarDropdown/CalendarDropdown.jsx
+++ src/components/CalendarDropdown/CalendarDropdown.jsx
@@ -4,19 +4,32 @@ import styles from './CalendarDropdown.module.scss';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import useProductStore from '../../store/useProductStore';
-import { parseISO, isValid } from 'date-fns';
+import { parseISO, isValid, format } from 'date-fns';
+import { getProductsData } from '../../services/products';
+import useUserStore from '../../store/useUserStore';
 
 const CalendarDropdown = () => {
 	const [isOpen, setIsOpen] = useState(false);
 	const { setSelectedDate, currentDate } = useProductStore();
+	const { user } = useUserStore();
 
 	const toggleCalendar = () => {
 		setIsOpen((prev) => !prev);
 	};
 
-	const handleDateChange = (date) => {
+	const handleDateChange = async (date) => {
+		const dateStr = format(date, 'yyyy-MM-dd');
 		setSelectedDate(date);
 		setIsOpen(false);
+		if (!user?.uid) return;
+
+		const history = useProductStore.getState().dailyHistory[dateStr];
+		if (history) {
+			console.log('Dane z cache:', history);
+			return;
+		}
+
+		await getProductsData(user.uid, dateStr);
 	};
 
 	const parsedDate = parseISO(currentDate);
@@ -29,7 +42,11 @@ const CalendarDropdown = () => {
 			</div>
 			{isOpen && (
 				<div className={styles.dropdown}>
-					<Calendar onChange={handleDateChange} value={safeDate} />
+					<Calendar
+						onChange={handleDateChange}
+						value={safeDate}
+						maxDate={new Date()}
+					/>
 				</div>
 			)}
 		</div>
