8ef38e760718c3e9fafcecca60bbaf8b44487e5f
Angelika Musiał <angelikamusial@tlen.pl>
2025-07-24 14:57:44 +0200

    Integrate user state and product fetching into login and registration logic

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,6 +4,9 @@ import {
 	createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
+import useProductStore from '../store/useProductStore';
+import { getProductsData } from './products';
+import { format } from 'date-fns';
 
 export const loginUser = async (email, password) => {
 	const userCredential = await signInWithEmailAndPassword(
@@ -13,7 +16,17 @@ export const loginUser = async (email, password) => {
 	);
 	const user = userCredential.user;
 	const userDoc = await getDoc(doc(db, 'users', user.uid));
-	return { user, userData: userDoc.data() };
+	const userData = userDoc.data();
+
+	useUserStore.getState().setUser({
+		uid: user.uid,
+		email: user.email,
+		...userData,
+	});
+
+	await getProductsData(user.uid, format(new Date(), 'yyyy-MM-dd'));
+
+	return { user, userData };
 };
 
 export const registerUser = async ({ name, surname, email, password }) => {
@@ -24,5 +37,10 @@ export const registerUser = async ({ name, surname, email, password }) => {
 		email,
 		profileComplete: false,
 	});
+
+	useProductStore.getState().logout();
+
+	await getProductsData(user.uid, format(new Date(), 'yyyy-MM-dd'));
+
 	return user;
 };
