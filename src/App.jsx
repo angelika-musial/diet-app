6d1385258efe96493dc2e98f5c2dc79df90af4c5
Angelika Musiał <angelikamusial@tlen.pl>
2025-07-24 14:53:03 +0200

    add Toaster to App for global toast notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import RootLayout from './pages/RootLayout';
 import StartPage from './pages/StartPage/StartPage';
 import RegistrationPage from './pages/RegistrationPage';
-import MainPage from './pages/MainPage/MainPage'
+import MainPage from './pages/MainPage/MainPage';
 import ProfileSetup from './pages/ProfileSetup';
 import ProtectedRoutes from './utils/ProtectedRoutes';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <RootLayout />,
+		errorElement: <ErrorPage />,
 		children: [
 			{ index: true, element: <StartPage /> },
 			{ path: 'signup', element: <RegistrationPage /> },
@@ -26,7 +29,12 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-	return <RouterProvider router={router} />;
+	return (
+		<>
+			<RouterProvider router={router} />
+			<Toaster position='top-center' />
+		</>
+	);
 };
 
 export default App;
