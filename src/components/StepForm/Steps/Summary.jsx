c2b279cd37221bc01bb1be694385e318a4a06cd8
Angelika Musiał <angelikamusial@tlen.pl>
2025-07-24 14:51:35 +0200

    Improve Summary component UX and update description

--- src/components/StepForm/Steps/Summary.jsx
+++ src/components/StepForm/Steps/Summary.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Step from './Step';
 import { Loader } from 'lucide-react';
 import styles from '../StepForm.module.scss';
@@ -8,48 +9,49 @@ import { useNavigate } from 'react-router-dom';
 import { profileIsComplete } from '../../../services/profile';
 
 export default function Summary({ loading, tdee }) {
-	const navigate = useNavigate();
+	const [isCompleting, setIsCompleting] = useState(false);
 	const [user] = useAuthState(auth);
+	const navigate = useNavigate();
 
 	const handleComplete = async () => {
-		try {
-			await profileIsComplete(user.uid);
-
-			navigate('/main');
-		} catch (error) {
-			console.error('Failed to mark profile as complete:', error);
-		}
+		setIsCompleting(true);
+		await profileIsComplete(user.uid);
+		navigate('/main');
 	};
 
+	if (loading) {
+		return (
+			<Step title='Jeszcze tylko chwila!'>
+				<div className={styles.summaryContainer}>
+					<Loader size={50} className={styles.loaderIcon} />
+					<p>Wyliczamy teraz Twoje dzienne zapotrzebowanie kaloryczne...</p>
+				</div>
+			</Step>
+		);
+	}
+
 	return (
-		<>
-			{loading ? (
-				<Step title='Jeszcze tylko chwila!'>
-					<div className={styles.summaryContainer}>
-						<Loader size={50} className={styles.loaderIcon} />
-						<p>Wyliczamy teraz Twoje dzienne zapotrzebowanie kaloryczne...</p>
-					</div>
-				</Step>
-			) : (
-				<Step title='Oto Twoje podsumowanie!'>
-					<div className={styles.summaryContainer}>
-						<p>
-							Twoje zapotrzebowanie kaloryczne wynosi
-							<strong> {tdee} kcal</strong>
-						</p>
-						<p>
-							Znajomość swojego dziennego zapotrzebowania kalorycznego pozwoli
-							Ci świadomie zarządzać dietą i osiągać zamierzone efekty. Teraz
-							wystarczy, że dobierzesz odpowiednie posiłki i będziesz cieszyć
-							się lepszym samopoczuciem oraz zdrowiem każdego dnia - i to bez
-							zbędnych wyrzeczeń!
-						</p>
-					</div>
-					<Button onClick={handleComplete} className={styles.completeButton}>
-						Gotowe
-					</Button>
-				</Step>
-			)}
-		</>
+		<Step title='Oto Twoje podsumowanie!'>
+			<div className={styles.summaryContainer}>
+				<p>
+					Twoje zapotrzebowanie kaloryczne wynosi
+					<strong> {tdee} kcal</strong>
+				</p>
+				<p>
+					Znajomość swojego dziennego zapotrzebowania kalorycznego pozwoli Ci
+					lepiej kontrolować dietę i osiągać założone cele. Dodając spożywane
+					produkty w ciągu dnia, będziesz na bieżąco monitorować kalorie i
+					makroskładniki, co pomoże Ci zachować świadomość żywieniową i
+					wprowadzać zdrowsze nawyki!
+				</p>
+			</div>
+			<Button
+				onClick={handleComplete}
+				className={styles.completeButton}
+				disabled={isCompleting}
+			>
+				Zaczynajmy
+			</Button>
+		</Step>
 	);
 }
