05952b016acf23f74b0085c9673b9852cadfa862
Angelika Musiał <angelikamusial@tlen.pl>
2025-07-24 15:17:12 +0200

    Update MainPage: fetch daily products, show greeting and AddMealCard only for today, add MainPage styling

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -5,21 +5,37 @@ import useUserStore from '../../store/useUserStore';
 import useProductStore from '../../store/useProductStore';
 import styles from './MainPage.module.scss';
 import { useEffect } from 'react';
+import { getProductsData } from '../../services/products';
 
 export default function MainPage() {
 	const { user } = useUserStore();
-	const { checkDateAndReset } = useProductStore();
+	const { checkDateAndReset, isToday, currentDate } = useProductStore();
 
 	useEffect(() => {
 		checkDateAndReset();
-	}, []);
+	}, [checkDateAndReset]);
+
+	useEffect(() => {
+		if (user.uid && currentDate) {
+			getProductsData(user.uid, currentDate);
+		}
+	}, [user.uid, currentDate]);
 
 	return (
 		<>
-			<Header />
-			<h2 className={styles.greeting}>Witaj {user?.name}</h2>
-			<DailyGoalPanel />
-			<AddMealCard />
+			<div className={styles.backgroundImage}></div>
+			<div className={styles.content}>
+				<Header />
+				<div className={isToday ? styles.greetingWrapper : styles.noGreeting}>
+					{isToday && (
+						<h2 className={styles.greeting}>
+							Dobrze Cię widzieć, {user.name}!
+						</h2>
+					)}
+				</div>
+				<DailyGoalPanel />
+				{isToday && <AddMealCard />}
+			</div>
 		</>
 	);
 }
